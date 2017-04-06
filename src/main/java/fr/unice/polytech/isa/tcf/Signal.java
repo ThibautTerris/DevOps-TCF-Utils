50466b384b7051792e8cf7919e870b6c5fa9652a
Thibaut TERRIS <thibaut.terris@gmail.com>
2017-04-06 14:44:08 +0200

    Init depot

---
+++ src/main/java/fr/unice/polytech/isa/tcf/Signal.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.isa.tcf;
+
+public class Signal {
+
+    public static final String SELECTED = "SELECTED";
+    public static final String UNKOWN = "UNKNOWN";
+    public static final String ADDED = "ADDED";
+    public static final String REMOVED = "REMOVED";
+    public static final String EXISTING = "EXISTING";
+    public static final String ORDERED = "ORDERED";
+    public static final String PAYMENT_ERROR = "PAYMENT_ERROR";
+    public static final String EMPTY_CART = "EMPTY_CART";
+}
